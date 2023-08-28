99abd2cd8a5f53e67c638c01108d4d0737408cd5
MyFirstGitEver <fordevok@gmail.com>
2023-08-28 12:44:25 +0700

    Initial Commit

---
+++ DictionaryApp/DictionaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryAppApp.swift
+//  DictionaryApp
+//
+//  Created by FVFH4069Q6L7 on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
